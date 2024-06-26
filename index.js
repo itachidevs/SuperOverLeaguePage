40c23f9dc2b76510745b5c47d5d51e7231a9365d
babybhavani <nxtwave@ip-10-2-57-185.ap-south-1.compute.internal>
2024-06-26 16:46:42 +0000

    creted repository

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className='container'>
+    <h1 className='heading'>Super Over League</h1>
+    <div className='inner'>
+      <img
+        className='image'
+        src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+      />
+      <img
+        className='image'
+        src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+      />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
